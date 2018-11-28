9795b7ba1690e1b0c6c1dec284df4967437d9d27
Kesley Ribeiro <kesley.developer@icloud.com>
2018-11-28 17:35:52 -0500

    Beginning the project with Xcode

---
+++ Rideshare Car/Rideshare Car/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rideshare Car
+//
+//  Created by Kesley Ribeiro on 28/11/18.
+//  Copyright © 2018 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Rideshare Car/Rideshare CarTests/Rideshare_CarTests.swift
@@ -0,0 +1,34 @@
+//
+//  Rideshare_CarTests.swift
+//  Rideshare CarTests
+//
+//  Created by Kesley Ribeiro on 28/11/18.
+//  Copyright © 2018 Kesley Ribeiro. All rights reserved.
+//
+
+import XCTest
+@testable import Rideshare_Car
+
+class Rideshare_CarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
