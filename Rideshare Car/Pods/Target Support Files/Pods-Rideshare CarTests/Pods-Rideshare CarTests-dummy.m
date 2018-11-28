ec36d9cc127d3f74d0f520275038100c420b593b
Kesley Ribeiro <kesley.developer@icloud.com>
2018-11-28 17:46:48 -0500

    Added CocoaPods to project

---
+++ Rideshare Car/Pods/Target Support Files/Pods-Rideshare CarTests/Pods-Rideshare CarTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rideshare_CarTests : NSObject
+@end
+@implementation PodsDummy_Pods_Rideshare_CarTests
+@end
---
+++ Rideshare Car/Pods/Target Support Files/Pods-Rideshare CarUITests/Pods-Rideshare CarUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rideshare_CarUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Rideshare_CarUITests
+@end
